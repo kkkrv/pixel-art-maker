21f23e1ba20fa62a3c8deab4100187c449b199fc
Olga Kokkareva <olgakokkareva08055@gmail.com>
2021-02-07 20:36:49 +0300

    add pallete

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
-let canvasField = document.getElementById("canvas");
+const canvasField = document.getElementById("canvas");
+const paletteField = document.getElementById('palette');
 
+let activeColor = 'red';
+
+canvasField.addEventListener('click', (e) => {
+    let el = e.target;
+    if (el.className === 'element') el.style.backgroundColor = activeColor;
+})
+
+paletteField.addEventListener('click', (e) => {
+    let el = e.target;
+    if (el.className === 'colorField') activeColor = el.style.backgroundColor;
+})
+
+function createPalette() {
+    const colors = [ 'maroon', 'darkred', 'red', 'indianred', 'tomato', 'lightcoral', 'lightpink', 'lightsalmon',
+        'sandybrown', 'peru', 'orangered', 'orange', 'goldenrod','gold',  'khaki', 'yellowgreen', 'olive',
+        'green', 'seagreen', 'mediumaquamarine', 'darkcyan', 'cornflowerblue', 'blue', 'darkblue', 'blueviolet', 'slateblue',
+        'darkorchid', 'darkmagenta', 'black', 'gray', 'lightgrey', 'white'];
+    for (let color of colors) {
+        console.log(color);
+        let colorField = document.createElement('div');
+        colorField.className = 'colorField';
+        // colorField.id = color;
+        colorField.style.backgroundColor = color;
+        paletteField.appendChild(colorField);
+    }
+}
 
 function createGrid() {
     let width = 800,
@@ -23,4 +50,5 @@ function createGrid() {
     }
 }
 
-createGrid()
+createGrid();
+createPalette();
