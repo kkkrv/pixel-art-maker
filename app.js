7ee1839deefd083a8d369da874a2cd52df8af82a
Olga Kokkareva <olgakokkareva08055@gmail.com>
2021-02-09 22:47:36 +0300

    add saving in localstorage

--- app.js
+++ app.js
@@ -1,30 +1,76 @@
-const canvasField = document.getElementById("canvas");
+const canvasField = document.getElementById("gridCanvas");
 const paletteField = document.getElementById('palette');
-
+const indicator = document.getElementById("currentColor");
+const saveButton = document.getElementById("saveButton");
+const clearButton = document.getElementById("clearButton");
+const customColor = document.getElementById("customColor");
 let activeColor = 'red';
+indicator.style.backgroundColor = activeColor;
+let isDrawing = false;
 
-canvasField.addEventListener('click', (e) => {
-    let el = e.target;
-    if (el.className === 'element') el.style.backgroundColor = activeColor;
+// ============== EVENT LISTENERS =====================
+window.addEventListener('load', () => {
+    getDrawingFromStorage();
 })
 
 paletteField.addEventListener('click', (e) => {
     let el = e.target;
-    if (el.className === 'colorField') activeColor = el.style.backgroundColor;
+    if (el.className === 'colorField' && el.tagName !== "input") {
+        activeColor = el.style.backgroundColor;
+        indicator.style.backgroundColor = activeColor;
+    }
 })
 
+customColor.addEventListener("change", (e) => {
+    let el = e.target;
+    activeColor = el.value;
+    indicator.style.backgroundColor = activeColor;
+})
+
+// Add the event listeners for mousedown, mousemove, and mouseup
+canvasField.addEventListener('mousedown', e => {let el = e.target;
+    e.preventDefault();
+    if (el.className === 'element') el.style.backgroundColor = activeColor;
+    isDrawing = true;
+});
+
+canvasField.addEventListener('mouseover', e => {
+    e.preventDefault();
+    if (isDrawing === true) {
+        let el = e.target;
+        if (el.className === 'element') el.style.backgroundColor = activeColor;
+    }
+});
+
+window.addEventListener('mouseup', () => {
+    if (isDrawing === true) {
+        isDrawing = false;
+    }
+});
+
+clearButton.addEventListener('click', () => {
+    let pixels = document.getElementsByClassName("element");
+    for(let pixel of pixels) {
+        if (pixel.style.backgroundColor != "") pixel.style.backgroundColor = "";
+    }
+})
+
+saveButton.addEventListener('click', () => {
+    setDrawingInStorage();
+})
+// ===================== FUNCTIONS =======================
+
 function createPalette() {
-    const colors = [ 'maroon', 'darkred', 'red', 'indianred', 'tomato', 'lightcoral', 'lightpink', 'lightsalmon',
-        'sandybrown', 'peru', 'orangered', 'orange', 'goldenrod','gold',  'khaki', 'yellowgreen', 'olive',
+    const colors = [ 'maroon', 'darkred', 'red', 'tomato', 'lightcoral', 'lightpink', 'lightsalmon',
+        'sandybrown', 'sienna', 'peru', 'orangered', 'orange', 'goldenrod','gold',  'yellowgreen', 'olive',
         'green', 'seagreen', 'mediumaquamarine', 'darkcyan', 'cornflowerblue', 'blue', 'darkblue', 'blueviolet', 'slateblue',
         'darkorchid', 'darkmagenta', 'black', 'gray', 'lightgrey', 'white'];
     for (let color of colors) {
         console.log(color);
         let colorField = document.createElement('div');
         colorField.className = 'colorField';
-        // colorField.id = color;
         colorField.style.backgroundColor = color;
-        paletteField.appendChild(colorField);
+        paletteField.prepend(colorField);
     }
 }
 
@@ -35,19 +81,48 @@ function createGrid() {
     let ratioW = Math.floor(width / size),
         ratioH = Math.floor(height / size);
 
-    let parent = document.getElementById("gridCanvas");
-    parent.style.width = String(ratioW  * size);
-    parent.style.height = String(ratioH  * size);
-
+    canvasField.style.width = String(ratioW  * size);
+    canvasField.style.height = String(ratioH  * size);
 
     for (let i = 0; i < ratioH; i++) {
         for(let p = 0; p < ratioW; p++){
             let el = document.createElement("div");
             el.className = "element";
-            el.id = `el_${i}_${p}`;
-            parent.appendChild(el);
+            el["x-position"] = `${i}`;
+            el["y-position"] = `${p}`;
+            canvasField.appendChild(el);
         }
     }
+}
+
+// ================== WORK WITH LOCALSTORAGE ======================
+
+function setDrawingInStorage() {
+    let pixels = document.getElementsByClassName("element");
+    let drawing = [];
+    for(let pixel of pixels) {
+        let colorBlock = {};
+        if (pixel.style.backgroundColor) {
+            colorBlock.x = pixel["x-position"];
+            colorBlock.y = pixel["y-position"];
+            colorBlock.color = pixel.style.backgroundColor;
+            drawing.push(colorBlock);
+        }
+    }
+    localStorage.setItem('drawing', JSON.stringify(drawing));
+}
+
+function getDrawingFromStorage() {
+    let drawing = JSON.parse(localStorage.getItem('drawing'));
+    let pixels = document.getElementsByClassName("element");
+    for (let pixel of pixels) {
+        for (let savedBlock of drawing) {
+            if (Number(pixel["x-position"]) === Number(savedBlock.x) && Number(pixel["y-position"]) === Number(savedBlock.y)) {
+                pixel.style.backgroundColor = savedBlock.color;
+            }
+        }
+    }
+
 }
 
 createGrid();
